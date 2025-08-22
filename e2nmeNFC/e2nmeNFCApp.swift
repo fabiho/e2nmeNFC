0e64d9b5c99176ede0d6cc62d78acb3886f22911
Fabian Hofer <hofer-93@gmx.de>
2025-08-22 09:07:46 +0200

    Initial Commit

---
+++ e2nmeNFC/e2nmeNFCApp.swift
@@ -0,0 +1,17 @@
+//
+//  e2nmeNFCApp.swift
+//  e2nmeNFC
+//
+//  Created by Fabian Hofer on 22.08.25.
+//
+
+import SwiftUI
+
+@main
+struct e2nmeNFCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ e2nmeNFCTests/e2nmeNFCTests.swift
@@ -0,0 +1,17 @@
+//
+//  e2nmeNFCTests.swift
+//  e2nmeNFCTests
+//
+//  Created by Fabian Hofer on 22.08.25.
+//
+
+import Testing
+@testable import e2nmeNFC
+
+struct e2nmeNFCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
